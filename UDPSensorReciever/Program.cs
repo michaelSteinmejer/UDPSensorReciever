9204369664f494e6e94522732fd88bb32fe0de32
michaelSteinmejer <33892912+michaelSteinmejer@users.noreply.github.com>
2018-12-26 12:40:23 +0100

    Initial commit

---
+++ UDPSensorReciever/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace UDPSensorReciever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //double value=0;
+
+            UdpClient udpServer = new UdpClient(7000);
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 7000);
+          
+            try
+            {
+                while (true)
+                {
+                    
+                    Byte[] receiveBytes = udpServer.Receive(ref RemoteIpEndPoint);
+                    
+                    string receivedData = Encoding.ASCII.GetString(receiveBytes);
+                    Console.WriteLine(receivedData);
+                    //if (receivedData.ToLower().Contains("stop"))
+                    //{
+                    //    break;
+                    //}
+                    //string[] datarecieved = receivedData.Split('\n');
+                  
+                    //var CO = datarecieved[3].Split(':');
+                    //string blah = CO[1];
+                    //double co = double.Parse(blah);
+                    //var NOx = datarecieved[4].Split(':');
+                    //var nox = double.Parse(NOx[1]);
+                    
+
+                    //value = value + (co+nox);
+
+
+                   // Console.WriteLine(co);
+                   //Console.WriteLine($"CO: {co} \r\nNOx: {nox} ");
+                 }
+                //Console.Clear();
+                //Console.WriteLine($"Sum: {value}");
+                //Console.WriteLine("Press any key to continue....");
+                //Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+
+        }
+    }
+}
